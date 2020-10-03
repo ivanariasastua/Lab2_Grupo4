8ca3e011596d6f80c5de9694bb99feac2d50edd8
YohellBuzo <54456019+YohellBuzo@users.noreply.github.com>
2020-10-02 20:03:00 -0600

    Controller de viviendas

--- laboratorio/src/main/java/org/una/laboratorio/controllers/ViviendaController.java
+++ laboratorio/src/main/java/org/una/laboratorio/controllers/ViviendaController.java
@@ -5,10 +5,40 @@
  */
 package org.una.laboratorio.controllers;
 
-/**
- *
- * @author Dios
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.una.laboratorio.services.IViviendaService;
+
+
+@RestController
+@RequestMapping("/tramites_registrados")
 public class ViviendaController {
     
+    @Autowired
+    private IViviendaService viviendaService;
+    
+    @GetMapping()
+    public @ResponseBody
+    ResponseEntity<?> findAll() {
+        try{
+            return new ResponseEntity(viviendaService.findAll(), HttpStatus.OK);
+        }catch(Exception ex){
+            return new ResponseEntity<>(ex.getClass(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable(value = "id") Long id) {
+        try{
+            return new ResponseEntity<>(viviendaService.findById(id), HttpStatus.OK);
+        }catch(Exception ex){
+            return new ResponseEntity<>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
