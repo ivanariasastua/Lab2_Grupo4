2f6fc429d3018056cea61098a9a1d1eb51c0779e
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-10-02 20:04:27 -0600

    Poblacion entidad

---
+++ laboratorio/src/main/java/org/una/laboratorio/dto/PoblacionDTO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.laboratorio.dto;
+
+
+/**
+ *
+ * @author Ivan Josué Arias Astua
+ */
+public class PoblacionDTO {
+    private Long id;
+    private String provincia;
+    private String canton;
+    private String distrito;
+    private String codigoPostal;
+}
